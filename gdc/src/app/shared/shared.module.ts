981142efbbe3c5b1342647db0064cd653ce217b7
Yannick Dubreuil <yannick@arturr.com>
2023-02-07 16:42:16 +0100

    Added navbar not finished

--- gdc/src/app/shared/shared.module.ts
+++ gdc/src/app/shared/shared.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
-  declarations: [
-    NavbarComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    NavbarComponent
-  ]
+  declarations: [NavbarComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonToggleModule],
+  exports: [NavbarComponent],
 })
 export class SharedModule {}

55954524c0ae7a3e96088015020e5e80f9478cc8
claragohier <clara.gohier@gmail.com>
2023-02-08 12:26:46 +0100

    form ajout conges css en cours

--- gdc/src/app/services/absence/absence.service.ts
+++ gdc/src/app/services/absence/absence.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Absence } from 'src/app/models/Absence.model';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -11,6 +12,10 @@ export class AbsenceService {
 
   getAbsences(id: Number) {
     return this.http.get(`${environment.API_URL}absences/user/${id}`);
+  }
+
+  addAbsence(absence: Absence){
+    return this.http.post(`${environment.API_URL}absences`, absence);
   }
 
 }
